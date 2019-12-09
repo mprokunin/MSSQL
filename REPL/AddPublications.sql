89312effb622d29ec14be6a8b9e0773b0421b01f
mprokunin <56938241+mprokunin@users.noreply.github.com>
2019-12-09 17:59:34 +0300

    Rename AddPublications.sql to REPL/AddPublications.sql

---
+++ REPL/AddPublications.sql
@@ -0,0 +1,9 @@
+﻿use TDB
+go
+
+-- Prepare table for publication (add primary key)
+alter table Table_1 alter column col2 int not null
+go
+ALTER TABLE dbo.Table_1   
+ADD CONSTRAINT PK_Table_1 PRIMARY KEY CLUSTERED (Col2);  
+GO  
