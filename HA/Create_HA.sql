5a94a4be340375f22452df7cc6738ff7bbf01ab2
mprokunin <56938241+mprokunin@users.noreply.github.com>
2019-12-10 14:17:24 +0300

    Create Create_HA.sql

---
+++ HA/Create_HA.sql
@@ -0,0 +1,219 @@
+--- YOU MUST EXECUTE THE FOLLOWING SCRIPT IN SQLCMD MODE.
+:Connect TSQL03
+
+IF (SELECT state FROM sys.endpoints WHERE name = N'Hadr_endpoint') <> 0
+BEGIN
+	ALTER ENDPOINT [Hadr_endpoint] STATE = STARTED
+END
+
+
+GO
+
+use [master]
+
+GO
+
+GRANT CONNECT ON ENDPOINT::[Hadr_endpoint] TO [ICATON\SQLUSER]
+
+GO
+
+:Connect TSQL03
+
+IF EXISTS(SELECT * FROM sys.server_event_sessions WHERE name='AlwaysOn_health')
+BEGIN
+  ALTER EVENT SESSION [AlwaysOn_health] ON SERVER WITH (STARTUP_STATE=ON);
+END
+IF NOT EXISTS(SELECT * FROM sys.dm_xe_sessions WHERE name='AlwaysOn_health')
+BEGIN
+  ALTER EVENT SESSION [AlwaysOn_health] ON SERVER STATE=START;
+END
+
+GO
+
+:Connect TSQL05
+
+IF (SELECT state FROM sys.endpoints WHERE name = N'Hadr_endpoint') <> 0
+BEGIN
+	ALTER ENDPOINT [Hadr_endpoint] STATE = STARTED
+END
+
+
+GO
+
+use [master]
+
+GO
+
+GRANT CONNECT ON ENDPOINT::[Hadr_endpoint] TO [ICATON\SQLUSER]
+
+GO
+
+:Connect TSQL05
+
+IF EXISTS(SELECT * FROM sys.server_event_sessions WHERE name='AlwaysOn_health')
+BEGIN
+  ALTER EVENT SESSION [AlwaysOn_health] ON SERVER WITH (STARTUP_STATE=ON);
+END
+IF NOT EXISTS(SELECT * FROM sys.dm_xe_sessions WHERE name='AlwaysOn_health')
+BEGIN
+  ALTER EVENT SESSION [AlwaysOn_health] ON SERVER STATE=START;
+END
+
+GO
+
+:Connect TSQL03
+
+USE [master]
+
+GO
+
+CREATE AVAILABILITY GROUP [testhadr.aton.global]
+WITH (AUTOMATED_BACKUP_PREFERENCE = NONE,
+DB_FAILOVER = ON,
+DTC_SUPPORT = PER_DB)
+FOR DATABASE [ReportServer$SQL2008], [ReportServer$SQL2008TempDB], [TDB]
+REPLICA ON N'TSQL03\SQL2008' WITH (ENDPOINT_URL = N'TCP://tsql03.aton.global:5022', FAILOVER_MODE = MANUAL, AVAILABILITY_MODE = ASYNCHRONOUS_COMMIT, BACKUP_PRIORITY = 50, SEEDING_MODE = MANUAL, PRIMARY_ROLE(READ_ONLY_ROUTING_LIST = (N'TSQL05\SQL2008')), SECONDARY_ROLE(READ_ONLY_ROUTING_URL = N'TSQL05:1433', ALLOW_CONNECTIONS = ALL)),
+	N'TSQL05\SQL2008' WITH (ENDPOINT_URL = N'TCP://tsql05.aton.global:5022', FAILOVER_MODE = MANUAL, AVAILABILITY_MODE = ASYNCHRONOUS_COMMIT, BACKUP_PRIORITY = 50, SEEDING_MODE = MANUAL, PRIMARY_ROLE(READ_ONLY_ROUTING_LIST = (N'TSQL03\SQL2008')), SECONDARY_ROLE(READ_ONLY_ROUTING_URL = N'TSQL05:1433', ALLOW_CONNECTIONS = ALL));
+
+GO
+
+:Connect TSQL03
+
+USE [master]
+
+GO
+
+ALTER AVAILABILITY GROUP [testhadr.aton.global]
+ADD LISTENER N'testhadr' (
+WITH IP
+((N'172.17.6.74', N'255.255.255.0'),
+(N'172.17.16.74', N'255.255.255.0'))
+)
+, PORT=1433);
+
+GO
+
+:Connect TSQL05
+
+ALTER AVAILABILITY GROUP [testhadr.aton.global] JOIN;
+
+GO
+
+:Connect TSQL05
+
+
+-- Wait for the replica to start communicating
+begin try
+declare @conn bit
+declare @count int
+declare @replica_id uniqueidentifier 
+declare @group_id uniqueidentifier
+set @conn = 0
+set @count = 30 -- wait for 5 minutes 
+
+if (serverproperty('IsHadrEnabled') = 1)
+	and (isnull((select member_state from master.sys.dm_hadr_cluster_members where upper(member_name COLLATE Latin1_General_CI_AS) = upper(cast(serverproperty('ComputerNamePhysicalNetBIOS') as nvarchar(256)) COLLATE Latin1_General_CI_AS)), 0) <> 0)
+	and (isnull((select state from master.sys.database_mirroring_endpoints), 1) = 0)
+begin
+    select @group_id = ags.group_id from master.sys.availability_groups as ags where name = N'testhadr.aton.global'
+	select @replica_id = replicas.replica_id from master.sys.availability_replicas as replicas where upper(replicas.replica_server_name COLLATE Latin1_General_CI_AS) = upper(@@SERVERNAME COLLATE Latin1_General_CI_AS) and group_id = @group_id
+	while @conn <> 1 and @count > 0
+	begin
+		set @conn = isnull((select connected_state from master.sys.dm_hadr_availability_replica_states as states where states.replica_id = @replica_id), 1)
+		if @conn = 1
+		begin
+			-- exit loop when the replica is connected, or if the query cannot find the replica status
+			break
+		end
+		waitfor delay '00:00:10'
+		set @count = @count - 1
+	end
+end
+end try
+begin catch
+	-- If the wait loop fails, do not stop execution of the alter database statement
+end catch
+ALTER DATABASE [ReportServer$SQL2008] SET HADR AVAILABILITY GROUP = [testhadr.aton.global];
+
+GO
+
+:Connect TSQL05
+
+
+-- Wait for the replica to start communicating
+begin try
+declare @conn bit
+declare @count int
+declare @replica_id uniqueidentifier 
+declare @group_id uniqueidentifier
+set @conn = 0
+set @count = 30 -- wait for 5 minutes 
+
+if (serverproperty('IsHadrEnabled') = 1)
+	and (isnull((select member_state from master.sys.dm_hadr_cluster_members where upper(member_name COLLATE Latin1_General_CI_AS) = upper(cast(serverproperty('ComputerNamePhysicalNetBIOS') as nvarchar(256)) COLLATE Latin1_General_CI_AS)), 0) <> 0)
+	and (isnull((select state from master.sys.database_mirroring_endpoints), 1) = 0)
+begin
+    select @group_id = ags.group_id from master.sys.availability_groups as ags where name = N'testhadr.aton.global'
+	select @replica_id = replicas.replica_id from master.sys.availability_replicas as replicas where upper(replicas.replica_server_name COLLATE Latin1_General_CI_AS) = upper(@@SERVERNAME COLLATE Latin1_General_CI_AS) and group_id = @group_id
+	while @conn <> 1 and @count > 0
+	begin
+		set @conn = isnull((select connected_state from master.sys.dm_hadr_availability_replica_states as states where states.replica_id = @replica_id), 1)
+		if @conn = 1
+		begin
+			-- exit loop when the replica is connected, or if the query cannot find the replica status
+			break
+		end
+		waitfor delay '00:00:10'
+		set @count = @count - 1
+	end
+end
+end try
+begin catch
+	-- If the wait loop fails, do not stop execution of the alter database statement
+end catch
+ALTER DATABASE [ReportServer$SQL2008TempDB] SET HADR AVAILABILITY GROUP = [testhadr.aton.global];
+
+GO
+
+:Connect TSQL05
+
+
+-- Wait for the replica to start communicating
+begin try
+declare @conn bit
+declare @count int
+declare @replica_id uniqueidentifier 
+declare @group_id uniqueidentifier
+set @conn = 0
+set @count = 30 -- wait for 5 minutes 
+
+if (serverproperty('IsHadrEnabled') = 1)
+	and (isnull((select member_state from master.sys.dm_hadr_cluster_members where upper(member_name COLLATE Latin1_General_CI_AS) = upper(cast(serverproperty('ComputerNamePhysicalNetBIOS') as nvarchar(256)) COLLATE Latin1_General_CI_AS)), 0) <> 0)
+	and (isnull((select state from master.sys.database_mirroring_endpoints), 1) = 0)
+begin
+    select @group_id = ags.group_id from master.sys.availability_groups as ags where name = N'testhadr.aton.global'
+	select @replica_id = replicas.replica_id from master.sys.availability_replicas as replicas where upper(replicas.replica_server_name COLLATE Latin1_General_CI_AS) = upper(@@SERVERNAME COLLATE Latin1_General_CI_AS) and group_id = @group_id
+	while @conn <> 1 and @count > 0
+	begin
+		set @conn = isnull((select connected_state from master.sys.dm_hadr_availability_replica_states as states where states.replica_id = @replica_id), 1)
+		if @conn = 1
+		begin
+			-- exit loop when the replica is connected, or if the query cannot find the replica status
+			break
+		end
+		waitfor delay '00:00:10'
+		set @count = @count - 1
+	end
+end
+end try
+begin catch
+	-- If the wait loop fails, do not stop execution of the alter database statement
+end catch
+ALTER DATABASE [TDB] SET HADR AVAILABILITY GROUP = [testhadr.aton.global];
+
+GO
+
+
+GO
+
+
