08be26434584d9a990ed08f37919bd5294c9b1a3
mprokunin <56938241+mprokunin@users.noreply.github.com>
2020-05-06 12:06:57 +0300

    Rename DropSubscription.sql to REPL/DropSubscription.sql

---
+++ REPL/DropSubscription.sql
@@ -0,0 +1,20 @@
+﻿-- Dropping the transactional subscriptions
+use [TDB]
+sp_helpsubscription
+go
+exec sp_dropsubscription @publication = N'TDB_Pub', @subscriber = N'tsql05\bo', @destination_db = N'TDB', @article = N'all'
+GO
+
+-- Dropping the transactional articles
+use [TDB]
+exec sp_dropsubscription @publication = N'TDB_Pub', @article = N'Table_1', @subscriber = N'all', @destination_db = N'all'
+GO
+use [TDB]
+exec sp_droparticle @publication = N'TDB_Pub', @article = N'Table_1', @force_invalidate_snapshot = 1
+GO
+
+-- Dropping the transactional publication
+use [TDB]
+exec sp_droppublication @publication = N'TDB_Pub'
+GO
+
