778a4ebcd8881b433b0ccffda22c603c8539e6b6
mprokunin <56938241+mprokunin@users.noreply.github.com>
2020-05-06 12:06:22 +0300

    Rename DropPublication.sql to REPL/DropPublication.sql

---
+++ REPL/DropPublication.sql
@@ -0,0 +1,28 @@
+﻿-- Dropping the transactional publication
+use [mif]
+exec sp_droppublication @publication = N'Mif_Pub'
+GO
+
+
+
+/****** Scripting removing replication objects. Script Date: 25.04.2019 17:24:09 ******/
+/****** Please Note: For security reasons, all password parameters were scripted with either NULL or an empty string. ******/
+
+-- Disabling the replication database
+use master
+exec sp_replicationdboption @dbname = N'TDB', @optname = N'publish', @value = N'false'
+GO
+
+-- Disabling the replication database
+use master
+exec sp_replicationdboption @dbname = N'TDB', @optname = N'merge publish', @value = N'false'
+GO
+
+
+/****** Uninstalling the server as a Distributor. Script Date: 25.04.2019 17:24:09 ******/
+use master
+exec sp_dropdistributor @no_checks = 1, @ignore_distributor = 1
+GO
+
+
+exec msdb..sp__dbinfo 1
