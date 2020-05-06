2bf88f8a4d015c479ef5b0701bb480e2a79f72e3
mprokunin <56938241+mprokunin@users.noreply.github.com>
2020-05-06 12:10:25 +0300

    Rename NewSubscription_1.sql to REPL/NewSubscription_1.sql

---
+++ REPL/NewSubscription_1.sql
@@ -0,0 +1,7 @@
+﻿-----------------BEGIN: Script to be run at Publisher 'TSQL03\SQL2008'-----------------
+use [TDB]
+exec sp_addsubscription @publication = N'TDB_publication', @subscriber = N'tsql03\bo', @destination_db = N'TDB', @subscription_type = N'Push', @sync_type = N'automatic', @article = N'all', @update_mode = N'read only', @subscriber_type = 0
+exec sp_addpushsubscription_agent @publication = N'TDB_publication', @subscriber = N'tsql03\bo', @subscriber_db = N'TDB', @job_login = null, @job_password = null, @subscriber_security_mode = 0, @subscriber_login = N'sa', @subscriber_password = null, @frequency_type = 64, @frequency_interval = 0, @frequency_relative_interval = 0, @frequency_recurrence_factor = 0, @frequency_subday = 0, @frequency_subday_interval = 0, @active_start_time_of_day = 0, @active_end_time_of_day = 235959, @active_start_date = 20190409, @active_end_date = 99991231, @enabled_for_syncmgr = N'False', @dts_package_location = N'Distributor'
+GO
+-----------------END: Script to be run at Publisher 'TSQL03\SQL2008'-----------------
+
