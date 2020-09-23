fb619081174ce1e0dca70d036fdff1cf29856e33
mprokunin <56938241+mprokunin@users.noreply.github.com>
2020-09-23 07:39:18 +0300

    Update ADM.ManagePartitions.sql

--- JOBS/ADM.ManagePartitions.sql
+++ JOBS/ADM.ManagePartitions.sql
@@ -1,11 +1,11 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [ADM.ManagePartitions]    Script Date: 6/22/2020 1:43:24 PM ******/
+/****** Object:  Job [ADM.ManagePartitions]    Script Date: 9/23/2020 7:36:29 AM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Notifies]]    Script Date: 6/22/2020 1:43:24 PM ******/
+/****** Object:  JobCategory [[Notifies]]    Script Date: 9/23/2020 7:36:29 AM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Notifies]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Notifies]'
@@ -23,10 +23,10 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ADM.ManagePartitions',
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'[Notifies]', 
-		@owner_login_name=N'RIMOS_NT_01\MProkunin', 
+		@owner_login_name=N'sa', 
 		@notify_email_operator_name=N'SQLAdmin', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Add New Partition]    Script Date: 6/22/2020 1:43:24 PM ******/
+/****** Object:  Step [Add New Partition]    Script Date: 9/23/2020 7:36:30 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Add New Partition', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -39,7 +39,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Add New 
 		@os_run_priority=0, @subsystem=N'TSQL', 
 		@command=N'use [REN_LOG]
 go
-declare @path varchar(100) = ''J:\renlog.data\REN_LOG_'', @partname1 char(8), @partname2 char(8), @exestr varchar(max) = '''', @i int =1;
+declare @path varchar(100) = ''Q'' + '':\renlog.data\REN_LOG_'', @partname1 char(8), @partname2 char(8), @exestr varchar(max) = '''', @i int = 2;
 select @partname1 = convert(char(8), dateadd(dd, @i, getdate()), 112)
 select @partname2 = convert(char(8), dateadd(dd, @i+1, getdate()), 112)
 select @exestr = ''ALTER DATABASE [REN_LOG] ADD FILEGROUP [REN_LOG_FG_'' + @partname1 + '']''
@@ -58,7 +58,7 @@ go',
 		@database_name=N'master', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Drop Partition 1]    Script Date: 6/22/2020 1:43:24 PM ******/
+/****** Object:  Step [Drop Partition 1]    Script Date: 9/23/2020 7:36:30 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Drop Partition 1', 
 		@step_id=2, 
 		@cmdexec_success_code=0, 
@@ -85,11 +85,14 @@ go
 
 declare @MIN_RANGE sql_variant, @OLD_FG sysname, @EXESTR varchar(max)
 set @MIN_RANGE=(select top 1 boundary_value from PartitionRanges where partition_function=''Daily_PF'' and boundary_value is not null order by boundary_value)
-select convert(char(8), dateadd(dd, -1, convert(datetime, @MIN_RANGE)), 112)
+--select convert(char(8), dateadd(dd, -1, convert(datetime, @MIN_RANGE)), 112)
 select @EXESTR = ''Alter Partition function Daily_PF() merge range ('''''' + convert(varchar(23), @MIN_RANGE) + '''''')'' 
 exec (@EXESTR)
 --select  (@EXESTR)
 select @EXESTR = ''alter database REN_LOG remove file REN_LOG_'' + convert(char(8), dateadd(dd, -1, convert(datetime, @MIN_RANGE)), 112)
+--select  (@EXESTR)
+exec (@EXESTR)
+select @EXESTR = ''alter database REN_LOG remove FILEGROUP REN_LOG_FG_'' + convert(char(8), dateadd(dd, -1, convert(datetime, @MIN_RANGE)), 112)
 --select  (@EXESTR)
 exec (@EXESTR)
 go', 
@@ -108,7 +111,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Daily',
 		@freq_recurrence_factor=0, 
 		@active_start_date=20200319, 
 		@active_end_date=99991231, 
-		@active_start_time=300, 
+		@active_start_time=223002, 
 		@active_end_time=235959, 
 		@schedule_uid=N'8d8e1f2b-f48f-44c9-b3c0-f8fad8ed21b3'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
