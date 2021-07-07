5a314586c1834c0ad62d8d4a49eadbcba297f9de
mprokunin <56938241+mprokunin@users.noreply.github.com>
2021-07-07 14:44:09 +0300

    Create AssemblyInfo.sql

---
+++ CLR/AssemblyInfo.sql
@@ -0,0 +1,20 @@
+select top 100 db_name(db_id) as 'DB',* from sys.dm_clr_appdomains 
+
+-- The following example shows how to view the details of an AppDomain for a given assembly:
+select appdomain_id, creation_time, db_id, user_id, state  
+from sys.dm_clr_appdomains a  
+where appdomain_address =   
+(select appdomain_address   
+ from sys.dm_clr_loaded_assemblies  
+   where assembly_id = 500);
+
+-- The following example shows how to view all assemblies in a given AppDomain:
+select a.name, a.assembly_id, a.permission_set_desc, a.is_visible, a.create_date, l.load_time   
+from sys.dm_clr_loaded_assemblies as l   
+inner join sys.assemblies as a  
+on l.assembly_id = a.assembly_id  
+where l.appdomain_address =   
+(select appdomain_address   
+from sys.dm_clr_appdomains  
+where appdomain_id = 4);
+
