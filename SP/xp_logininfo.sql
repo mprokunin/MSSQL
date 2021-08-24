4836a24090781403a10b113c958e95b3aee49b90
mprokunin <56938241+mprokunin@users.noreply.github.com>
2021-08-24 12:37:19 +0300

    Update xp_logininfo.sql

--- SP/xp_logininfo.sql
+++ SP/xp_logininfo.sql
@@ -2,32 +2,6 @@ xp_logininfo [ [ @acctname = ] 'account_name' ]
 [ , [ @option = ] 'all' | 'members' ] 
 [ , [ @privilege = ] variable_name OUTPUT]
 
-exec xp_logininfo @acctname = 'RIMOS_NT_01\Front_analytics_Access_DB_RO', @option = 'members'
+exec xp_logininfo @acctname = 'DOMAIN\Front_analytics_Access_RA', @option = 'members'
 
 
-RIMOS_NT_01\AAntipov	user	user	RIMOS_NT_01\AAntipov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\AGladysheva	user	user	RIMOS_NT_01\AGladysheva	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\APrivalov	user	user	RIMOS_NT_01\APrivalov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\ATrubnikov	user	user	RIMOS_NT_01\ATrubnikov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\AVardanyan	user	user	RIMOS_NT_01\AVardanyan	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\DShushpanov	user	user	RIMOS_NT_01\DShushpanov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\EMagsumov	user	user	RIMOS_NT_01\EMagsumov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\ESlousch	user	user	RIMOS_NT_01\ESlousch	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\GBatyushkov	user	user	RIMOS_NT_01\GBatyushkov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\KKrotova	user	user	RIMOS_NT_01\KKrotova	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\KZabelov	user	user	RIMOS_NT_01\KZabelov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\MBelskikh	user	user	RIMOS_NT_01\MBelskikh	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\MRadchenko	user	user	RIMOS_NT_01\MRadchenko	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\MYuchkin	user	user	RIMOS_NT_01\MYuchkin	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\NikolIr	user	user	RIMOS_NT_01\NikolIr	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\NovikVy	user	user	RIMOS_NT_01\NovikVy	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\NShulyapova	user	user	RIMOS_NT_01\NShulyapova	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\PalkiNa	user	user	RIMOS_NT_01\PalkiNa	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\PisarMa	user	user	RIMOS_NT_01\PisarMa	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\RZhdanko	user	user	RIMOS_NT_01\RZhdanko	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\STormosova1	user	user	RIMOS_NT_01\STormosova1	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\SukhoIl1	user	user	RIMOS_NT_01\SukhoIl1	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\SYurasov	user	user	RIMOS_NT_01\SYurasov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\VTimashinov	user	user	RIMOS_NT_01\VTimashinov	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\YBritova	user	user	RIMOS_NT_01\YBritova	RIMOS_NT_01\Front_analytics_Access_DB_RO
-RIMOS_NT_01\YGolubev	user	user	RIMOS_NT_01\YGolubev	RIMOS_NT_01\Front_analytics_Access_DB_RO
