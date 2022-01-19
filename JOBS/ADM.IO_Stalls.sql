8daac1ab1012e1864c6e9fcb417189f37ea7307b
mprokunin <56938241+mprokunin@users.noreply.github.com>
2022-01-19 12:47:46 +0300

    Update ADM.IO_Stalls.sql

--- JOBS/ADM.IO_Stalls.sql
+++ JOBS/ADM.IO_Stalls.sql
@@ -31,16 +31,17 @@ GO
 ALTER TABLE [dbo].[io_stalls] ADD  DEFAULT (getdate()) FOR [DT]
 GO
 
+--alter table [dbo].[io_stalls] add [num_of_bytes_read] [numeric](19, 0) NULL
+--alter table [dbo].[io_stalls] add [num_of_bytes_written] [numeric](19, 0) NULL
 
----------------------------
 USE [msdb]
 GO
 
-/****** Object:  Job [ADM.io_stalls]    Script Date: 14.03.2019 10:54:59 ******/
+/****** Object:  Job [ADM.io_stalls]    Script Date: 19.01.2022 11:39:44 ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Notifies]]    Script Date: 14.03.2019 10:54:59 ******/
+/****** Object:  JobCategory [[Notifies]]    Script Date: 19.01.2022 11:39:44 ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Notifies]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Notifies]'
@@ -52,15 +53,16 @@ DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ADM.io_stalls', 
 		@enabled=1, 
 		@notify_level_eventlog=0, 
-		@notify_level_email=0, 
+		@notify_level_email=2, 
 		@notify_level_netsend=0, 
 		@notify_level_page=0, 
 		@delete_level=0, 
 		@description=N'Fix io stalls per whole server', 
 		@category_name=N'[Notifies]', 
-		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'prokunin@ffin.ru', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [io_stalls]    Script Date: 14.03.2019 10:54:59 ******/
+/****** Object:  Step [io_stalls]    Script Date: 19.01.2022 11:39:44 ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'io_stalls', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -72,22 +74,18 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'io_stall
 		@retry_interval=0, 
 		@os_run_priority=0, @subsystem=N'TSQL', 
 		@command=N'----- Sender & Recipients
-
-declare 	@MailProfile sysname =  ''SQLIT201\OKB''
-	, @Recip varchar(max) = ''SQLAdmin@aton.ru'' 
---	, @Recip varchar(max) = ''mikhail.prokunin@aton.ru'' 
---	, @CopyRecip varchar(max) = ''_finsupport@aton.ru;Nikolay.Mezhenskiy@aton.ru''
---	, @CopyRecip varchar(max) = ''_finsupport@aton.ru''
-	, @CopyRecip varchar(max) = ''mikhail.prokunin@aton.ru''
+declare 	@MailProfile sysname =  ''MyProfile''
+	, @Recip varchar(max) = ''prokunin@abc.ru'' 
+	, @CopyRecip varchar(max) = ''prokunin@abc.ru''
 
 insert into master.dbo.io_stalls ([Database Name], [avg_read_stall_ms] ,[avg_write_stall_ms] ,[avg_io_stall_ms], [File Size (MB)],
-[physical_name], [type_desc], [io_stall_read_ms], [num_of_reads], [io_stall_write_ms], [num_of_writes], [io_stalls], 
+[physical_name], [type_desc], [io_stall_read_ms], [num_of_reads], [num_of_bytes_read], [io_stall_write_ms], [num_of_writes], [num_of_bytes_written], [io_stalls], 
 [total_io], [Resource Governor Total Read IO Latency (ms)], [Resource Governor Total Write IO Latency (ms)])
 SELECT DB_NAME(fs.database_id) AS [Database Name], CAST(fs.io_stall_read_ms/(1.0 + fs.num_of_reads) AS NUMERIC(10,1)) AS [avg_read_stall_ms],
 CAST(fs.io_stall_write_ms/(1.0 + fs.num_of_writes) AS NUMERIC(10,1)) AS [avg_write_stall_ms],
 CAST((fs.io_stall_read_ms + fs.io_stall_write_ms)/(1.0 + fs.num_of_reads + fs.num_of_writes) AS NUMERIC(10,1)) AS [avg_io_stall_ms],
-CONVERT(DECIMAL(18,2), mf.size/128.0) AS [File Size (MB)], mf.physical_name, mf.type_desc, fs.io_stall_read_ms, fs.num_of_reads, 
-fs.io_stall_write_ms, fs.num_of_writes, fs.io_stall_read_ms + fs.io_stall_write_ms AS [io_stalls], fs.num_of_reads + fs.num_of_writes AS [total_io],
+CONVERT(DECIMAL(18,2), mf.size/128.0) AS [File Size (MB)], mf.physical_name, mf.type_desc, fs.io_stall_read_ms, fs.num_of_reads, fs.num_of_bytes_read,
+fs.io_stall_write_ms, fs.num_of_writes, fs.num_of_bytes_written, fs.io_stall_read_ms + fs.io_stall_write_ms AS [io_stalls], fs.num_of_reads + fs.num_of_writes AS [total_io],
 io_stall_queued_read_ms AS [Resource Governor Total Read IO Latency (ms)], io_stall_queued_write_ms AS [Resource Governor Total Write IO Latency (ms)] 
 
 FROM sys.dm_io_virtual_file_stats(null,null) AS fs
@@ -102,7 +100,7 @@ select @DT2 = max(DT) from master..io_stalls
 select @DT1 = max(DT) from master..io_stalls where DT < @DT2
 --drop table #Results 
 
-select [DT], [Database Name], [physical_name], [io_stall_read_ms], [num_of_reads], [io_stall_write_ms], [num_of_writes] 
+select [DT], [Database Name], [physical_name], [io_stall_read_ms], [num_of_reads], [num_of_bytes_read], [io_stall_write_ms], [num_of_writes], [num_of_bytes_written]
 into #Results
 from master..io_stalls 
 where DT = @DT1 -- and [Database Name] in (''tempdb'', ''p_ner'', ''P_AST'')
@@ -114,31 +112,35 @@ declare
 @physical_name nvarchar(250), 
 @io_stall_read_ms numeric(19,0), 
 @num_of_reads bigint,
+@num_of_bytes_read numeric(19,0),
 @io_stall_write_ms numeric(19,0),
 @num_of_writes bigint,
+@num_of_bytes_written numeric(19,0),
 @EXESTR varchar(max)
 
 declare stalls_cur cursor for 
-select [Database Name], [physical_name], [io_stall_read_ms], [num_of_reads], [io_stall_write_ms], [num_of_writes] 
+select [Database Name], [physical_name], [io_stall_read_ms], [num_of_reads], [num_of_bytes_read], [io_stall_write_ms], [num_of_writes], [num_of_bytes_written]
 from master..io_stalls 
 where DT = @DT2 -- and [Database Name] in (''tempdb'', ''p_ner'', ''P_AST'')
 
 OPEN stalls_cur  
 
-FETCH NEXT FROM stalls_cur INTO @Database_Name, @physical_name, @io_stall_read_ms, @num_of_reads, 
-	@io_stall_write_ms, @num_of_writes 
+FETCH NEXT FROM stalls_cur INTO @Database_Name, @physical_name, @io_stall_read_ms, @num_of_reads, @num_of_bytes_read,
+	@io_stall_write_ms, @num_of_writes, @num_of_bytes_written
 
 WHILE @@FETCH_STATUS = 0  
 BEGIN  
 	select @EXESTR = ''update #Results set [io_stall_read_ms] = ('' + convert(nvarchar(21), + @io_stall_read_ms) 	+ '' - [io_stall_read_ms]) ,''
 	+ ''[io_stall_write_ms] = ('' + convert(nvarchar(21), + @io_stall_write_ms) 	+ '' - [io_stall_write_ms]), ''
 	+ ''[num_of_reads] = ('' + convert(nvarchar(21), + @num_of_reads) 	+ '' - [num_of_reads]), ''
-	+ ''[num_of_writes] = ('' + convert(nvarchar(21), + @num_of_writes) 	+ '' - [num_of_writes])''
+	+ ''[num_of_bytes_read] = ('' + convert(nvarchar(21), + @num_of_bytes_read) 	+ '' - [num_of_bytes_read]), ''
+	+ ''[num_of_writes] = ('' + convert(nvarchar(21), + @num_of_writes) 	+ '' - [num_of_writes]),''
+	+ ''[num_of_bytes_written] = ('' + convert(nvarchar(21), + @num_of_bytes_written) + '' - [num_of_bytes_written]) ''
 	+ '' where [Database Name] = '''''' + @Database_Name + '''''' and [physical_name] = '''''' + @physical_name + ''''''''
 --	select @EXESTR
 	exec (@EXESTR)
-	FETCH NEXT FROM stalls_cur INTO @Database_Name, @physical_name, @io_stall_read_ms, @num_of_reads, 
-		@io_stall_write_ms, @num_of_writes 
+	FETCH NEXT FROM stalls_cur INTO @Database_Name, @physical_name, @io_stall_read_ms, @num_of_reads, @num_of_bytes_read,
+		@io_stall_write_ms, @num_of_writes, @num_of_bytes_written
 END   
 CLOSE stalls_cur;  
 
@@ -146,7 +148,7 @@ DEALLOCATE stalls_cur;
 
 declare @html varchar(max)
 select @html = ''<html><body><h3>IO Stalls on '' + @@servername + '' from '' + convert(varchar(19), @DT1) + '' to '' + convert(varchar(19), @DT2) +
-''</h3><table border=1><tr><td>Database Name</td><td>Physical Name</td><td>Read stalls ms</td><td>Write Stalls ms</td><td>Num of Reads</td><td>Num of Writes</td></tr>''
+''</h3><table border=1><tr><th>Database Name</th><th>Physical Name</th><th>Read stalls ms</th><th>Write Stalls ms</th><th>Num of Reads</th><th>Read MB</th><th>Num of Writes</th><th>Written MB</th></tr>''
 select @html = @html + ''<tr><td>'' + [Database Name] +''</td><td>''+ [physical_name] +''</td><td>'' +
 CASE 
 		WHEN num_of_reads = 0 THEN ''0'' 
@@ -155,8 +157,10 @@ END +''</td><td>''+
 CASE 
 		WHEN num_of_writes = 0 THEN ''0'' 
 		ELSE convert(varchar(21),convert(numeric(10,2),(io_stall_write_ms/num_of_writes)))
-END +''</td><td>'' +
-	convert(varchar(21),[num_of_reads]) +''</td><td>'' + convert(varchar(21),[num_of_writes]) + ''</td></tr>''
+END +''</td><td>'' 
+	+ convert(varchar(21),[num_of_reads]) + ''</td><td>'' + convert(varchar(21),convert(numeric(19,0),[num_of_bytes_read]/(1024*1024))) + ''</td><td>'' +
+	+ convert(varchar(21),[num_of_writes]) +''</td><td>'' + convert(varchar(21),convert(numeric(19,0),[num_of_bytes_written]/(1024*1024)))
+	+ ''</td></tr>''
 from #Results
 order by [Database Name], [physical_name]
 
@@ -181,15 +185,15 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'every hou
 		@enabled=1, 
 		@freq_type=4, 
 		@freq_interval=1, 
-		@freq_subday_type=8, 
-		@freq_subday_interval=1, 
+		@freq_subday_type=4, 
+		@freq_subday_interval=30, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=0, 
 		@active_start_date=20190218, 
 		@active_end_date=99991231, 
-		@active_start_time=100, 
+		@active_start_time=600, 
 		@active_end_time=235959, 
-		@schedule_uid=N'bab76e78-a618-4108-8300-34e6887687d6'
+		@schedule_uid=N'7b6824fd-2345-4e99-b237-e2b65a73c7ed'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
@@ -199,4 +203,3 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 GO
-
