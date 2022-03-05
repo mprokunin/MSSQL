ecce3d7630d744acefd0c33dbed489519c82d85c
mprokunin <56938241+mprokunin@users.noreply.github.com>
2022-03-05 10:39:12 +0300

    Create CopyFiles.ps1

---
+++ PS/CopyFiles.ps1
@@ -0,0 +1,37 @@
+$srcpath = "F:\BACKUP\DBS-PURCB\DAB1\FULL"
+$dstpath = "\\1.1.1.1\Backup\DIB\PURCB\FULL"
+
+$logfile = "C:\ADM\CopyFULL.log"
+
+Function LogWrite
+{
+   Param ([string]$logstring)
+   Add-content $Logfile -value $logstring
+}
+
+Start-Transcript -Path $logfile
+
+$src = Get-ChildItem -Recurse -path $srcpath
+$dst = Get-ChildItem -Recurse -path $dstpath
+
+#Compare-Object -ReferenceObject $src -DifferenceObject $dst
+
+$i = 0
+foreach ($file in $src) {
+    $targetFile = $dst | where Name -eq $file.Name
+
+    # this copies files which do not exist in the target
+    if ($file.LastWriteTime -gt $targetFile.LastWriteTime) {
+        Copy-Item $file.FullName $dstpath
+#      	LogWrite "$file.FullName copied to $dstpath"
+
+    Write-Host "$file.FullName copied to $dstpath"
+
+	  $i += 1
+    	if ($i -gt 4) {
+	    break
+    	}
+    }
+}
+
+Stop-Transcript
