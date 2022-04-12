47dbcbb7d6868b8031c9ae4747f6ba8d7ca9f9b9
mprokunin <56938241+mprokunin@users.noreply.github.com>
2022-04-12 12:18:19 +0300

    Update sp_addpushsubscription_agent.sql

--- REPL/sp_addpushsubscription_agent.sql
+++ REPL/sp_addpushsubscription_agent.sql
@@ -1,4 +1,4 @@
 EXEC sp_helpsubscriberinfo;  
-exec sp_addpushsubscription_agent @publication = N'AtonBase_Pub', @subscriber = N'SQL206', @subscriber_db = N'AB', @job_login = null, @job_password = null, @subscriber_security_mode = 1, @frequency_type = 64, @frequency_interval = 1, @frequency_relative_interval = 1, @frequency_recurrence_factor = 0, @frequency_subday = 4, @frequency_subday_interval = 5, @active_start_time_of_day = 0, @active_end_time_of_day = 235959, @active_start_date = 0, @active_end_date = 0, @dts_package_location = N'Distributor'
+exec sp_addpushsubscription_agent @publication = N'ABCBase_Pub', @subscriber = N'SQL206', @subscriber_db = N'AB', @job_login = null, @job_password = null, @subscriber_security_mode = 1, @frequency_type = 64, @frequency_interval = 1, @frequency_relative_interval = 1, @frequency_recurrence_factor = 0, @frequency_subday = 4, @frequency_subday_interval = 5, @active_start_time_of_day = 0, @active_end_time_of_day = 235959, @active_start_date = 0, @active_end_date = 0, @dts_package_location = N'Distributor'
 GO
 
